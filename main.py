c2a68d4b2fae24a5eb0c73d33b0617289b37ab07
yizhou82 <yizhou11182@gmail.com>
2023-07-27 18:45:06 -0700

    Added homepage

--- main.py
+++ main.py
@@ -1,7 +1,77 @@
 from tkinter import *
-window=Tk()
-lbl=Label(window, text="EEG Record", font=("Helvetica", 16))
-lbl.place(x=60, y=50)
-window.title('EEG Record')
+import tkinter as tk
+
+class HomePage(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.title("EEG Record")
+        #self.geometry("800x600")
+
+        button_back = tk.Button(self, text="Back", padx=5, pady=2, command=lambda: [self.show_pageMain(), button_back.pack_forget()])
+
+        # Create the frames for different pages
+        self.pageMain = tk.Frame(self)
+        self.pageJoystick = tk.Frame(self)
+        self.pageOpenBCI = tk.Frame(self)
+        self.pageRecord = tk.Frame(self)
+
+        # Create and place widgets in each page
+        self.create_widgets_main(button_back)
+        self.create_widgets_joystick()
+        self.create_widgets_openbci()
+        self.create_widgets_record()
+
+        self.show_page(self.pageMain)
+
+    def create_widgets_main(self, button_back):
+        label = tk.Label(self.pageMain, text="EEG Data Recording", font=("Arial", 12))
+        label.pack(pady=(10,40))
+
+        # Buttons for navigation
+
+        buttonframe = tk.Frame(self.pageMain)
+        buttonframe.rowconfigure(0, weight=1)
+        buttonframe.rowconfigure(1, weight=1)
+        buttonframe.rowconfigure(2, weight=1)
+
+        btn1 = tk.Button(buttonframe, text="Joystick", font=("Arial", 8), command=lambda: [button_back.pack(side="left", anchor="nw"), self.show_page(self.pageJoystick)])
+        btn1.grid(row=0, column=0, pady=5, sticky="nsew")
+
+        btn2 = tk.Button(buttonframe, text="OpenBCI Headset", font=("Arial", 8), command=lambda: [button_back.pack(side="left", anchor="nw"), self.show_page(self.pageOpenBCI)])
+        btn2.grid(row=1, column=0, pady=5, sticky="nsew")
+
+        btn3 = tk.Button(buttonframe, text="Start recording", font=("Arial", 8), command=lambda: [button_back.pack(side="left", anchor="nw"), self.show_page(self.pageRecord)])
+        btn3.grid(row=2, column=0, pady=5, sticky="nsew")
+
+        buttonframe.pack()
+    
+    def create_widgets_joystick(self):
+        label = tk.Label(self.pageJoystick, text="Joystick", font=("Arial", 12))
+        label.pack(pady=(10,40))
+
+    def create_widgets_openbci(self):
+        label = tk.Label(self.pageOpenBCI, text="Open BCI Headset", font=("Arial", 12))
+        label.pack(pady=(10,40))
+
+    def create_widgets_record(self):
+        label = tk.Label(self.pageRecord, text="Recording", font=("Arial", 12))
+        label.pack(pady=(10,40))
+
+    def show_page(self, page):
+        # Hide the main page and show the selected page
+        self.pageMain.pack_forget()
+        page.pack()
+
+    def show_pageMain(self):
+        # Hide all pages and show the main page
+        self.pageRecord.pack_forget()
+        self.pageJoystick.pack_forget()
+        self.pageOpenBCI.pack_forget()
+        self.pageMain.pack()
+
+window= HomePage()
+#lbl=Label(window, text="EEG Record", font=("Helvetica", 16))
+#lbl.place(x=60, y=50)
+#window.title('EEG Record')
 window.geometry("300x200+10+10")
 window.mainloop()
